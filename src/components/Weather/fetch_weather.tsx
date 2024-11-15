89801caeff2adc5b999c909d26aaf568ac66b4f0
Vartika Gupta <vartikag.13@gmail.com>
2024-11-15 12:02:03 -0500

    fix for eslint issues

--- src/components/Weather/fetch_weather.tsx
+++ src/components/Weather/fetch_weather.tsx
@@ -26,22 +26,10 @@ const WeatherAPI = () => {
             const url = 'https://api.open-meteo.com/v1/forecast';
             const responses = await fetchWeatherApi(url, params);
 
-            // Helper function to form time ranges
-            const range = (start: number, stop: number, step: number) =>
-                Array.from(
-                    { length: (stop - start) / step },
-                    (_, i) => start + i * step
-                );
-
             // Process first location. Add a for-loop for multiple locations or weather models
             const response = responses[0];
 
-            // Attributes for timezone and location
             const utcOffsetSeconds = response.utcOffsetSeconds();
-            const timezone = response.timezone();
-            const timezoneAbbreviation = response.timezoneAbbreviation();
-            const latitude = response.latitude();
-            const longitude = response.longitude();
 
             const current = response.current()!;
 
